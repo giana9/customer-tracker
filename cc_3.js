7096051e55e4443f277d35a10894cc9dd56a9bf8
Giana Cresta <crestagiana@gmail.com>
2026-05-31 22:01:04 -0400

    created customer array

---
+++ cc_3.js
@@ -0,0 +1,18 @@
+// Customer Array
+let customers = [
+{
+    name: "John Smith",
+    email: "john@gmail.com",
+    purchases: ["Popcorn", "Pepsi"]
+},
+{
+    name: "Poppy Lee",
+    email: "poppy@gmail.com",
+    purchases: ["Mint Ice Cream", "Milk Chocolate", "Whipped Cream"]
+},
+{
+    name: "Kira Tracey",
+    email: "kira@gmail.com",
+    purchases: ["Peanuts", "Watermelon"]
+}
+                ];
