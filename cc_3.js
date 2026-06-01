844bcdb385e1e1fa5de64ab2837d7f81c329cb61
Giana Cresta <crestagiana@gmail.com>
2026-05-31 22:12:43 -0400

    adding and removing customer info

--- cc_3.js
+++ cc_3.js
@@ -16,3 +16,13 @@ let customers = [
     purchases: ["Peanuts", "Watermelon"]
 }
                 ];
+
+// Adding a new customer
+customers.push({
+    name: "Mike Cerrel",
+    email: "mike@gmail.com",
+    purchases: ["Avocado", "Garlic"]
+});
+
+// Removing the first customer
+customers.shift();
